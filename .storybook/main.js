a9645f2b9d3ef934d11552b0c52365e1ed40b86b
João Victor <jovictor.miguel@gmail.com>
2021-06-01 16:46:21 -0300

    refactor: update storybook to use newest features

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,9 +2,5 @@ module.exports = {
   stories: [
     "../src/components/**/stories.tsx",
   ],
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register"
-  ]
+  addons: ["@storybook/addon-essentials"]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,17 +1,21 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { withKnobs, text } from '@storybook/addon-knobs'
 
 import Main from '.'
 
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'Default title',
+    description: 'Default description'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'Description text')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+//Args can be used like this
+Basic.args = {
+  title: 'Component args title',
+  description: 'Component args description'
+}
+
+export const Default: Story = (args) => <Main {...args} />
